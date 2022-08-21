2f41184746fb89aef214829134ba3bff53c14b3b
prg0615 <parguzatt@gmail.com>
2022-08-21 18:45:03 +0200

    next.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
+import { server } from "../config"
 import Head from "next/head"
 import ArticleList from "../components/ArticleList"
 
 export default function Home({ articles }) {
   return (
     <div>
-      <Head>
-        <title>WebDev PRG</title>
-        <meta name="keywords" content="web dewelopment,programming" />
-      </Head>
-
       <ArticleList articles={articles} />
     </div>
   )
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return {
@@ -24,3 +20,14 @@ export const getStaticProps = async () => {
     },
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
