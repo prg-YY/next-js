0ec0444f618fccf6dab56bf9938b369ba0a1d317
prg0615 <parguzatt@gmail.com>
2022-09-23 08:54:58 +0200

    next.router

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>Products</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Home
